54a4f0839f5d8d8bdb3fe12e1c0fe17302e1b634
AltCommander <158271189+AltCommander@users.noreply.github.com>
2024-10-04 11:47:07 +0300

    Lesson5 update

--- lesson5/l5_HW_2.py
+++ lesson5/l5_HW_2.py
@@ -7,12 +7,15 @@ if 'a' in word and 'e' in word and 'i' in word and 'o' in word and 'u' in word:
 else:
     print(False)
 
-#Определяем частоту гласных букв в слове
+#Определяем частоту гласных и согласных букв в слове
 count_a = word.count('a')
 count_e = word.count('e')
 count_i = word.count('i')
 count_o = word.count('o')
 count_u = word.count('u')
+
+count_vowels = count_a + count_e + count_i + count_o + count_u
+count_constants = len(word) - count_vowels
 
 #Выводим полученные данные
 
@@ -21,3 +24,5 @@ print("Count of 'e':", count_e)
 print("Count of 'i':", count_i)
 print("Count of 'o':", count_o)
 print("Count of 'u':", count_u)
+print("Count of vowels:", count_vowels)
+print("Count of consonants:", count_constants)
