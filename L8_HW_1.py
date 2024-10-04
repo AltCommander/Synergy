73c774c32a564f15af44fb6fdf9dfcf1ff72c0a2
AltCommander <158271189+AltCommander@users.noreply.github.com>
2024-10-04 16:38:47 +0300

    Delete L8_HW_1.py

--- L8_HW_1.py
+++
@@ -1,9 +0,0 @@
-#Получаем число N и вводим числа
-n = int(input("Введите число N:"))
-res = []
-for i in range(n):
-    res.append(int(input("Введите число:")))
-
-#Выводим результат
-res.reverse()
-print(res)
