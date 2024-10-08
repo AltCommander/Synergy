4f2b7aa5c082cc5464f0f1067b5629fdf6e5836b
AltCommander <158271189+AltCommander@users.noreply.github.com>
2024-10-08 21:35:31 +0300

    Lesson15 upload

---
+++ lesson15/L15_HW_1.py
@@ -0,0 +1,12 @@
+class Transport:
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def print_info(self):
+        print(f"Название автомобиля: {self.name} Скорость: {self.max_speed} Пробег: {self.mileage}")
+
+Autobus = Transport("Renaul Logan", 180, 12)
+Autobus.print_info()
